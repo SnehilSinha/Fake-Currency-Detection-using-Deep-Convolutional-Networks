4009061afd28d9b7c47ee84af68a6d417271e01f
srujank98 <62565156+srujank98@users.noreply.github.com>
2021-06-16 04:59:10 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,12 @@
-import cv2
-import numpy as np
-import os
-from random import shuffle
-from tqdm import tqdm
+import cv2  # working with, mainly resizing, images
+import numpy as np  # dealing with arrays
+import os  # dealing with directories
+from random import shuffle  # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm  # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
 
-IMG_SIZE = 264
-noOfCols = 264
-noOfRows = 264
-TRAIN_DIR = '' #add path
+IMG_SIZE = 224
 
+##TRAIN_DIR = 'D:\\08\\Final Year Project Implementation\\Raw Dataset\\Trials\\'
 
 def label_img(img):
     word_label = img.split('_')[-2]
@@ -17,19 +15,51 @@ def label_img(img):
     elif word_label == '500fake' or word_label == '2000fake':
         return [0, 1]
 
+
 def create_train_data():
     training_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
-        label = label_img(img)
-        path = os.path.join(TRAIN_DIR, img)
-        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        img = cv2.medianBlur(img, 5)
-        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-        for angle in range(15, -16, -1):
-            rotationMatrix = cv2.getRotationMatrix2D((noOfCols/2, noOfRows/2), angle, 1)
-            imgRotated = cv2.warpAffine(img, rotationMatrix, (noOfCols, noOfRows))
-            training_data.append([np.array(imgRotated), np.array(label)])
+    for TRAIN_DIR in ['D:\\08\\Final Year Project Implementation\\Raw Dataset\\500_Real\\', 'D:\\08\\Final Year Project Implementation\\Raw Dataset\\500_Fake\\']:
+        for img in tqdm(os.listdir(TRAIN_DIR)):
+            label = label_img(img)
+            path = os.path.join(TRAIN_DIR, img)
+            
+            gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+            high_thresh, thresh_im = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            lowThresh = 0.5*high_thresh
+            blur = cv2.medianBlur(gray, 17)
+            
+            edge_img = cv2.Canny(blur,lowThresh*1.5,high_thresh*1.5)
+            pts = np.argwhere(edge_img>0)
+            try:
+                y1,x1 = pts.min(axis=0)
+                y2,x2 = pts.max(axis=0)
+            except ValueError:
+                print (img)
+                pass
+            
+            cropped = gray[y1:y2, x1:x2]
+            blur_cropped = cv2.medianBlur(cropped, 7)
+            blur_cropped = cv2.resize(blur_cropped , (IMG_SIZE, IMG_SIZE))
+            training_data.append([np.array(blur_cropped), np.array(label)])
+            
+            #Applying Salt and Pepper filter
+            s_vs_p = 0.5
+            amount = 0.02
+            for j in range(0,5):
+                sp_img = np.copy(blur_cropped)
+                # Salt mode
+                num_salt = np.ceil(amount * blur_cropped.size * s_vs_p)
+                coords = [np.random.randint(0, i - 1, int(num_salt)) for i in blur_cropped.shape]
+                sp_img[coords] = 255
+                # Pepper mode
+                num_pepper = np.ceil(amount* blur_cropped.size * (1. - s_vs_p))
+                coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in blur_cropped.shape]
+                sp_img[coords] = 0
+                training_data.append([np.array(sp_img), np.array(label)])
+            
+    shuffle(training_data)
     np.save('train_data.npy', training_data)
     return training_data
 
 train_data = create_train_data()
+print(len(train_data))
